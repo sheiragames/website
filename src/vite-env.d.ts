311edeb941f0b45136431afb4d157bb4ea3a65ba
sheiragames <sheira.games.dev@gmail.com>
2026-07-31 00:16:19 +0200

    48: fix testing functions (#10)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference -- vite's own scaffolding convention; client.d.ts has no exports to import, only ambient declarations
 /// <reference types="vite/client" />
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- declaration merging (augmenting Window) only works with `interface`, `type` cannot do this
+interface Window {
+	testLog: () => void;
+	testErrorLog: () => void;
+}
