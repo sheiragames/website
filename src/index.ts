eb71245522231aa4eb397f5fb673fc8eca0bc84b
sheiragames <sheira.games.dev@gmail.com>
2026-07-31 00:15:15 +0200

    48: fix testing functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import { logPageLoad } from "@/logging/send";
+import { logPageLoad, logTestEvent, logTestErrorEvent } from "@/logging/send";
 
 logPageLoad();
+
+// eslint-disable-next-line functional/immutable-data -- exposing DevTools-callable test triggers requires assigning to window
+window.testLog = logTestEvent;
+// eslint-disable-next-line functional/immutable-data -- same as above
+window.testErrorLog = logTestErrorEvent;
